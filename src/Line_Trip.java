d03dd601eb2589087954fab335ebdfc9da360df6
Praveen Verma <knowitspraveeno@gmail.com>
2026-01-04 08:58:59 +0530

    Line Trip

---
+++ src/Line_Trip.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Line_Trip {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t>0){
+            int n = sc.nextInt();
+            int x = sc.nextInt();
+            int[] arr = new int[n+1];
+
+            for(int i =1; i<n+1; i++){
+                arr[i] = sc.nextInt();
+            }
+            int ans = 0;
+            int d = x - arr[n];
+            ans = 2*d;
+
+            for(int i = 0; i<n; i++){
+                int diff = arr[i+1]-arr[i];
+                ans = Math.max(ans, diff);
+            }
+            System.out.println(ans);
+            t--;
+        }
+    }
+}
