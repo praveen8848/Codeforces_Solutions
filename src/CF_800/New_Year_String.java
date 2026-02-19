9842c936cf30329e4cd13f7012fc4a254450a3d0
Praveen Verma <knowitspraveeno@gmail.com>
2026-02-19 22:59:20 +0530

    Easy one

---
+++ src/CF_800/New_Year_String.java
@@ -0,0 +1,25 @@
+
+
+import java.util.Scanner;
+
+public class New_Year_String {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-->0){
+            int n  = sc.nextInt();
+            String s = sc.next();
+
+            if(s.contains("2026")){
+                System.out.println(0);
+            }
+            else if(s.contains("2025")){
+                System.out.println(1);
+            }
+            else {
+                System.out.println(0);
+            }
+        }
+    }
+}
