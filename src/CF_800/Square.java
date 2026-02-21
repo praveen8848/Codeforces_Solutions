86b0a1159b76f20d2a8a3adeef4e8fcdabb88218
Praveen Verma <knowitspraveeno@gmail.com>
2026-02-21 22:53:42 +0530

    Just found an easy one

---
+++ src/CF_800/Square.java
@@ -0,0 +1,24 @@
+
+
+import java.util.Scanner;
+
+public class Square {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-->0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int d = sc.nextInt();
+
+            if( a== b && b == c && c == d){
+                System.out.println("Yes");
+            }
+            else{
+                System.out.println("No");
+            }
+        }
+    }
+}
