88900d46135d5d2578df5f26ddd1b34c91a63abb
Praveen Verma <knowitspraveeno@gmail.com>
2026-02-23 23:25:17 +0530

    Easy One

---
+++ src/CF_800/Sublime_Sequence.java
@@ -0,0 +1,23 @@
+
+
+import java.util.Scanner;
+
+public class Sublime_Sequence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-->0) {
+            int x = sc.nextInt();
+            int n = sc.nextInt();
+
+            if (n % 2 == 0) {
+                System.out.println(0);
+            } else {
+                System.out.println(x);
+            }
+
+        }
+        sc.close();
+    }
+}
